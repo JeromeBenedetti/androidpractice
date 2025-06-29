340bb344fae1a4e6b11ca02a554dc243a310fb57
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-06-29 03:20:48 +0200

    style: center signature

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -69,7 +69,7 @@ fun GreetingText(
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End),
+                .align(alignment = Alignment.CenterHorizontally),
         )
     }
 }
